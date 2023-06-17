d4caa3bd49a74e2574b5c5a47cc98c74e1fed505
Alessandro Proto <alex@alexdevs.me>
2023-06-17 12:23:23 +0200

    Add espeak module for SC3

---
+++ modules/espeak.lua
@@ -0,0 +1,51 @@
+-- ESpeak module for Minit
+-- Copyright (C) 2023 AlexDevs
+-- This software is licensed under the MIT license.
+
+-- This module is made for SwitchCraft 3
+-- A Chatbox license is required to use this module
+-- Or use "guest" license
+
+local module = {
+    name = "espeak",
+}
+
+local username
+local function speak(message)
+    local url = "https://music.madefor.cc/tts?text=" .. textutils.urlEncode(message)
+    local response, err = http.get { url = url, binary = true }
+    if not response then error(err, 0) end
+
+    local speaker = peripheral.find("speaker")
+    local decoder = require("cc.audio.dfpwm").make_decoder()
+
+    while true do
+        local chunk = response.read(16 * 1024)
+        if not chunk then break end
+
+        local buffer = decoder(chunk)
+        while not speaker.playAudio(buffer) do
+            os.pullEvent("speaker_audio_empty")
+        end
+    end
+end
+
+local function run()
+    while true do
+        local _, user, msg = os.pullEvent("chat_ingame")
+        if user == username then
+            speak(msg)
+        end
+    end
+end
+
+function module.init(init)
+    init.addTask(run)
+end
+
+function module.setup(neural)
+    local meta = neural.getMetaOwner()
+    username = meta.name
+end
+
+return module
